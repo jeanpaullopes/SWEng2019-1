eeb6823be446ad27a21c54c279891e9635ec9f1d
Kevin Veiga <kevin.veiga@gmail.com>
2019-05-30 21:25:12 -0300

    exemplo de grid do bootstrap

--- src/component/Page/Home/Home.js
+++ src/component/Page/Home/Home.js
@@ -7,7 +7,11 @@ export const Home = () => {
         <main className="home content-padding-y">
             <div className="container">
                 <div className="row align-items-center justify-content-between no-gutters">
-                    <div className="col-10 col-lg-5">Home</div>
+                    <div className="col-12 col-sm-6 col-xl-4">teste 1</div>
+
+                    <div className="col-12 col-sm-6 col-xl-4">teste 2</div>
+
+                    <div className="col-12 col-sm-6 col-xl-4">teste 3</div>
                 </div>
             </div>
         </main>
