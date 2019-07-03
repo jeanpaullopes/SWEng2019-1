da1ab195a40e9b05bb840b940cfa08009826784d
Márcio Santos <marciofsctt@gmail.com>
2019-07-02 22:50:51 -0300

    Ajustes de layout

--- src/component/Page/Home/Home.js
+++ src/component/Page/Home/Home.js
@@ -7,27 +7,23 @@ export const Home = () => {
         <main className="home content-padding-y">
             <div className="container">
                 <div className="row align-items-center justify-content-between no-gutters">
+                    <div className="container">
+                        <div className="row slider-text align-items-center justify-content-center">
+                            <h1>Quer ir aonde?</h1>                            
+                        </div>
+                        <div className="row slider-text align-items-center justify-content-center">
+                            <p>Possuímos caronas de confiança, com milhares de clientes por todo o brasil!</p>
+                        </div>
+                    </div>
                 
-                        <div className="container">
-                            <div className="row slider-text align-items-center justify-content-center">
-                                <h1>Quer ir aonde?</h1>                            
-                            </div>
-                            <div className="row slider-text align-items-center justify-content-center">
-                                <p>Possuímos caronas de confiança, com milhares de clientes por todo o brasil!</p>
-                            </div>
+                    <div className="container">
+                        <div className="row slider-text align-items-center justify-content-center">
+                            <h1>Quer ir viajar?</h1>
                         </div>
-                    
-
-                    
-                        <div className="container">
-                            <div className="row slider-text align-items-center justify-content-center">
-                                <h1>Quer ir viajar?</h1>                            
-                            </div>
-                            <div className="row slider-text align-items-center justify-content-center">
-                                <p>Viaje para qualquer lugar do brasil, com rapidez e sem transtornos!</p>
-                            </div>
+                        <div className="row slider-text align-items-center justify-content-center">
+                            <p>Viaje para qualquer lugar do brasil, com rapidez e sem transtornos!</p>
                         </div>
-                   
+                    </div>
                 </div>
             </div>
         </main>
