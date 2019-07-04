dd6e1d67cbe32df6791ff1f6962e21b29220b93a
Márcio Santos <marciofsctt@gmail.com>
2019-07-04 02:24:31 -0300

    Ajuste no layout do perfil

--- src/component/Page/Perfil/Perfil.js
+++ src/component/Page/Perfil/Perfil.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import img from '../../../asset/image/me1.png';
 import './Perfil.scss';
 
 export const Perfil = () => {
@@ -7,7 +8,21 @@ export const Perfil = () => {
         <main className="perfil content-padding-y">
             <div className="container">
                 <div className="row align-items-center justify-content-between no-gutters">
-                    <div className="col-10 col-lg-5">Perfil</div>
+                    
+                    <div className="flex1">
+                        <img src={img}></img>
+                    </div>
+
+                    <div className="flex2">
+                        <label>Nome</label>
+                        <output>José da Silva</output>
+
+                        <label>E-mail</label>
+                        <output>josesilva@gmail.com</output>
+
+                        <label>Telefone</label>
+                        <output>(51)99999-9999</output>
+                    </div>
                 </div>
             </div>
         </main>
